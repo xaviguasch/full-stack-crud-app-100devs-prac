0428cdd6476807c17d6e1ea4f0a2562278961a15
xaviguasch <xaviguasch@gmail.com>
2022-07-19 19:29:39 +0200

    added POST route and installed body-parser and mongodb

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  const { name, quote } = req.body
+
+  console.log(req.body)
 })
 
 app.listen(3002, () => {
